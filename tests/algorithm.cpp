95443434c13d46100c66687962da29a3ae246865
MartijnTerpstra <m-terpstra@outlook.com>
2020-12-18 20:07:52 +0100

    catch2

--- tests/algorithm.cpp
+++ tests/algorithm.cpp
@@ -1,5 +1,6 @@
 #include <algorithm.hpp>
 
+#if 0
 #include <gtest/gtest.h>
 
 TEST(Saturate, OnlyGoodValues) {
@@ -13,3 +14,22 @@ TEST(Saturate, NegativeValuesShouldBeZero) {
 TEST(Saturate, MaxValueShouleBeOne) {
     ASSERT_EQ(mtmath::saturate(100), 1.f);
 }
+
+#else
+
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+
+TEST_CASE("Saturate: OnlyGoodValues", "[Algo]") {
+    REQUIRE(mtmath::saturate(0.5f) == 0.5f);
+}
+
+TEST_CASE("Saturate: NegativeValuesShouldBeZero", "[Algo]") {
+    REQUIRE(mtmath::saturate(-0.5f) == 0.f);
+}
+
+TEST_CASE("Saturate: MaxValueShouleBeOne", "[Algo]") {
+    REQUIRE(mtmath::saturate(100) == 1.f);
+}
+
+#endif
