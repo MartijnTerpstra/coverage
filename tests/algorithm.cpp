49d276efb50579d20aa2c40932a7b4cfd19c7595
MartijnTerpstra <m-terpstra@outlook.com>
2021-01-06 00:26:05 +0100

    subdirectory added

--- tests/algorithm.cpp
+++ tests/algorithm.cpp
@@ -3,16 +3,19 @@
 #if 0
 #include <gtest/gtest.h>
 
-TEST(Saturate, OnlyGoodValues) {
-    ASSERT_EQ(mtmath::saturate(0.5f), 0.5f);
+TEST(Saturate, OnlyGoodValues)
+{
+	ASSERT_EQ(mtmath::saturate(0.5f), 0.5f);
 }
 
-TEST(Saturate, NegativeValuesShouldBeZero) {
-    ASSERT_EQ(mtmath::saturate(-0.5f), 0.f);
+TEST(Saturate, NegativeValuesShouldBeZero)
+{
+	ASSERT_EQ(mtmath::saturate(-0.5f), 0.f);
 }
 
-TEST(Saturate, MaxValueShouleBeOne) {
-    ASSERT_EQ(mtmath::saturate(100), 1.f);
+TEST(Saturate, MaxValueShouleBeOne)
+{
+	ASSERT_EQ(mtmath::saturate(100), 1.f);
 }
 
 #else
@@ -20,16 +23,19 @@ TEST(Saturate, MaxValueShouleBeOne) {
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
 
-TEST_CASE("Saturate: OnlyGoodValues", "[Algo]") {
-    REQUIRE(mtmath::saturate(0.5f) == 0.5f);
+TEST_CASE("Saturate: OnlyGoodValues", "[Algo]")
+{
+	REQUIRE(mtmath::saturate(0.5f) == 0.5f);
 }
 
-TEST_CASE("Saturate: NegativeValuesShouldBeZero", "[Algo]") {
-    REQUIRE(mtmath::saturate(-0.5f) == 0.f);
+TEST_CASE("Saturate: NegativeValuesShouldBeZero", "[Algo]")
+{
+	REQUIRE(mtmath::saturate(-0.5f) == 0.f);
 }
 
-TEST_CASE("Saturate: MaxValueShouleBeOne", "[Algo]") {
-    REQUIRE(mtmath::saturate(100) == 1.f);
+TEST_CASE("Saturate: MaxValueShouleBeOne", "[Algo]")
+{
+	REQUIRE(mtmath::saturate(100) == 1.f);
 }
 
 #endif
